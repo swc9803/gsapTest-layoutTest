570c403ccb891c96591adb503709671ec33ecfbd
swc9803 <swc9803@gmail.com>
2022-01-28 22:28:52 +0900

    gsap 랜덤함수

--- src/pages/Test15.vue
+++ src/pages/Test15.vue
@@ -3,15 +3,15 @@
     <SpaceBackground />
     <div class="wrapper">
       <!-- 최성우 누르면 소개페이지로 -->
-      <p ref="mainText">끝없이 배움을 추구하는 개발자<b> 최성우 </b>입니다</p>
+      <p ref="mainText">끝없이 배움을 추구하는 웹퍼블리셔
+        <span v-for="star in stars" :key="star.id" class="twinkle" :ref="twinkle">★</span>
+        <b> 최성우 </b>입니다
+      </p>
       <p>hi</p>
     </div>
   </div>
   <div class="container">
     소개
-  </div>
-  <div class="container">
-    백그라운드 이미지 피자처럼
   </div>
   <div class="container">
     작업물
@@ -26,14 +26,45 @@ import { onMounted, ref } from 'vue'
 export default {
   setup () {
     const mainText = ref()
+    const array = ref([])
+    const twinkle = (el) => array.value.push(el)
+    const star = ref()
+    const stars = ref([{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }])
 
     onMounted(() => {
       gsap.from(mainText.value, {
-        opacity: 0, yPercent: -20, duration: 1, ease: 'ease-out', delay: 1
+        // opacity: 0, yPercent: -20, duration: 1, ease: 'ease-out', delay: 1
       })
+      for (var i = 0; i < 4; i++) {
+        gsap.set(array.value[i], {
+          xPercent: gsap.utils.random(0, 900),
+          yPercent: gsap.utils.random(0, 300)
+        })
+        gsap.to(array.value[i], {
+          // xPercent: gsap.utils.random(0, 900),
+          // yPercent: gsap.utils.random(0, 300),
+          opacity: 0,
+          scale: 2,
+          duration: 0.3,
+          yoyo: true,
+          onComplete () {
+            gsap.delayedCall(gsap.utils.random(2, 5, 0.1), () => this.restart())
+          }
+        })
+      }
+      // gsap.utils.toArray(array.value).forEach(function (star) {
+      //   gsap.to(star, {
+      //     opacity: 0,
+      //     scale: 2,
+      //     duration: 0.5,
+      //     repeat: -1,
+      //     repeatDelay: gsap.utils.random(1.5, 2, 0.1),
+      //     repeatRefresh: true
+      //   })
+      // })
     })
     return {
-      mainText
+      mainText, twinkle, star, stars, array
     }
   },
   components: {
@@ -61,7 +92,7 @@ export default {
   }
   .wrapper {
     position: relative;
-    padding: 6% 2%;
+    padding: 20% 2%;
     text-align: center;
     font-family: 'KOTRALEAP';
     p {
@@ -73,40 +104,15 @@ export default {
         color: rgb(255, 255, 175);
         transform-origin: center center;
         cursor: pointer;
-        transition: .3s;
+        transition: .5s;
         &:hover {
           color: rgb(255, 200, 80);
         }
-        &::before {
-          position: absolute;
-          content: '★';
-          color: white;
-          font-size: 0.3em;
-          margin: 1em 0 0 0.5em;
-          animation: twinkle 5s infinite;
-        }
-        &::after {
-          position: absolute;
-          content: '★';
-          color: white;
-          font-size: 0.3em;
-          margin: 2.5em 0 0 -4.2em;
-          animation: twinkle 5s infinite;
-        }
-        @keyframes twinkle {
-          0% {
-            opacity: 0; rotate: 0deg;
-          }
-          5% {
-            opacity: 1; transform: scale(1.3); rotate: 90deg;
-          }
-          10% {
-            opacity: 0;
-          }
-          100% {
-            opacity: 0;
-          }
-        }
+      }
+      .twinkle {
+        position: absolute;
+        pointer-events: none;
+        font-size: 0.3em;
       }
     }
   }
